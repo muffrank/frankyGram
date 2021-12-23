e7aaf7c8ca762215ecc5ae29cf608287d0437f70
Muhammad.umar <muhammad.umar@fiveriverstech.com>
2021-12-24 01:59:47 +0500

    profile page

---
+++ profile.js
@@ -0,0 +1,12 @@
+$("#posts").on("click", function (e) {
+  $(".section-common").removeClass("show");
+  // $("#s2c0").addClass("show");
+});
+// $("#videos").on("click", function (e) {
+//   $(".section-common").removeClass("show");
+//   $("#s2c1").addClass("show");
+// });
+// $("#taged").on("click", function (e) {
+//   $(".section-common").removeClass("show");
+//   $("#s2c2").addClass("show");
+// });
